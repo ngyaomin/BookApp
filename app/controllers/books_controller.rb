c506fe7d410d0bf37e8740071cd566217460df92
Unknown <ngyaomin@ymail.com>
2017-02-13 11:07:39 +0800

    allow non log in user to search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   # GET /books
   # GET /books.json
 
